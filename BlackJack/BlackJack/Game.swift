d3af98e2d57c2bb92cd35a78ebf65d4dec08486d
brendon cecilio <brendoncecilio@Brendons-MacBook-Pro.local>
2019-10-24 17:01:13 -0400

    intial code

---
+++ BlackJack/BlackJack/Game.swift
@@ -0,0 +1,64 @@
+//
+//  Game.swift
+//  BlackJack
+//
+//  Created by Brendon Cecilio on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    var deck = [Card]()
+    var player = Player.self
+    var hitPlayer = Bool()
+    var userHitMe = "hit me"
+    var userHitPass = "pass"
+    var userScore = 0
+    var compScore = 0
+    
+    // computed properties
+    var hasMoreCards: Bool {
+        return !deck.isEmpty
+    }
+    var randomCompScore: Int {
+        let randomInt = 0
+        return randomInt
+    }
+    
+    //METHODS
+    
+    func newGame(score: Player) {
+        userScore = 0
+        compScore = 0
+    }
+
+    
+    func stopHits(userPass: String, userScore: Int) {
+            if userPass == userHitPass {
+                if userScore > randomCompScore {
+                    print("You win!")
+                }
+            }
+        }
+    
+    func hitMe(userHit: String, score: Int) -> Int{
+        if userHit == userHitMe {
+            userScore += 1
+        }
+        return userScore
+    }
+    
+    func computorVsPlayer(compRandomInt: Int, userScore: Int) {
+        if randomCompScore > userScore {
+            print("You lose")
+        } else if userScore > randomCompScore {
+            print("You win")
+        }
+    }
+    
+//    func gameStatus(playerScore: Int, currentScore: Int) {
+//        if score
+//    }
+}
